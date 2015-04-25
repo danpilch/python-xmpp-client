0d0d631bc80c527f9163824868268e18a96b9317
dan <daniel.pilch@incopro.co.uk>
2015-04-25 13:44:15 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # python-xmpp-client
 Radically simple Python command line tool for interacting with XMPP servers to send notification messages.
+
+## Requirements
+
+Python 2.6+
+Python xmpppy library -> http://xmpppy.sourceforge.net/
+
+## Example Usage
+
+```
+python xmpp_notify.py --help
+
+age: xmpp_notify.py [-h] [-d HOST] [-u USER] [-pw PASSWORD]
+                      [-t [TO [TO ...]]] [-p PORT] [-m MESSAGE] [-de]
+
+A simple XMPP message sending application
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -d HOST, --host HOST  The XMPP hostname
+  -u USER, --user USER  The username to connect to the host and send message
+                        via
+  -pw PASSWORD, --password PASSWORD
+                        The password associated with the username
+  -t [TO [TO ...]], --to [TO [TO ...]]
+                        The recipient/s of the message (space separated a b c)
+  -p PORT, --port PORT  The port to connect via default: 5222
+  -m MESSAGE, --message MESSAGE
+                        The message to send
+  -de, --debug          Enable debug mode
+
+```
+
+## Todo:
+
+1) Support sending messages to groups.
+
+## License
+
+MIT / BSD
+
+## Author Information
+
+Created by [Dan Pilch](https://github.com/danpilch) [@danpilch](https://twitter.com/danpilch)
+
---
+++ xmpp_notify.py
@@ -0,0 +1,66 @@
+import argparse
+import xmpp
+import logging
+
+# Todo:
+# 1) Disable xmpp debug
+# 2) Support message sending to a group
+
+log = logging.getLogger()
+log.setLevel('INFO')
+
+class xmpp_notify(object):
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description="A simple XMPP message sending application")
+        self.parser.add_argument("-d", "--host", help="The XMPP hostname")
+        self.parser.add_argument("-u", "--user", help="The username to connect to the host and send message via")
+        self.parser.add_argument("-pw", "--password", help="The password associated with the username")
+        self.parser.add_argument("-t", "--to", nargs='*', help="The recipient/s of the message (space separated a b c)")
+        self.parser.add_argument("-p", "--port", default=5222, help="The port to connect via default: 5222")
+        self.parser.add_argument("-m", "--message", help="The message to send")
+        self.parser.add_argument("-de", "--debug", action="store_true", help="Enable debug mode")
+
+        self.args = self.parser.parse_args()
+
+    def send_message(self):
+        jid = xmpp.JID('{0}@{1}'.format(self.args.user, self.args.host))
+        user = self.args.user
+        host = self.args.host
+        port = self.args.port
+        password = self.args.password
+        message = self.args.message
+        
+        msg = xmpp.protocol.Message(body=message)
+       
+        try:
+            conn = xmpp.Client(jid.getDomain(), debug=self.args.debug)
+            conn.connect(server=(host, port))
+            conn.auth(user, password, 'xmpp_notify')
+            conn.sendInitPresence(requestRoster=0)
+        except Exception as e:
+            log.error('Error connecting to XMPP server: {0}'.format(e))
+            raise
+
+        log.info('Connected to XMPP server.')
+        msg.setType('chat')
+        
+        for recipient in self.args.to:
+            try:
+                msg.setTo('{0}@{1}'.format(recipient, host))
+                conn.send(msg)
+                log.info('Sent message to user: {0}'.format(recipient))
+            except Exception as e:
+                log.error('Error sending message to user: {0} {1}'.format(recipient, e))
+                continue
+        
+        conn.disconnect()
+        log.info('Disconnected from XMPP server.')
+
+def main():
+    logging.basicConfig()
+
+    mail = xmpp_notify()
+    mail.send_message()
+
+if __name__ == "__main__":
+    main()
